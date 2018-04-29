e99c8af817aade6c1baa6eff553454b4914943a0
Anna Fang <amf269@cornell.edu>
2018-04-29 13:53:59 -0400

    train_loader

--- cfar10.py
+++ cfar10.py
@@ -14,7 +14,7 @@ transform = transforms.Compose(
 #trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
 #                                        download=True, transform=transform)
 train_loader = torch.utils.data.DataLoader(
-    datasets.CIFAR10('./cifar-10-batches-py', train = True, transform = transform), 
+    torchvision.datasets.CIFAR10('./cifar-10-batches-py', download = True,train = True, transform = transform), 
     batch_size=4, shuffle=True, num_workers=2)
 
 testset = torchvision.datasets.CIFAR10(root='./cifar-10-batches-py', train=False,
@@ -24,28 +24,6 @@ test_loader = torch.utils.data.DataLoader(testset, batch_size=4,
 
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-########################################################################
-# Let us show some of the training images, for fun.
-
-import matplotlib.pyplot as plt
-import numpy as np
-
-def imshow(img):
-    img = img / 2 + 0.5     # unnormalize
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-
-
-# get some random training images
-dataiter = iter(trainloader)
-images, labels = dataiter.next()
-
-# show images
-imshow(torchvision.utils.make_grid(images))
-# print labels
-print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
-
 
 ########################################################################
 # 2. Define a Convolution Neural Network
@@ -63,19 +41,17 @@ class Net(nn.Module):
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10,20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
-        self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc1 = nn.Linear(320,50)
+        self.fc2 = nn.Linear(50,10)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(f.max_pool2d(self.conv1(x),2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
 
 
 net = Net()
@@ -101,7 +77,7 @@ optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 for epoch in range(2):  # loop over the dataset multiple times
 
     running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
+    for i, data in enumerate(train_loader, 0):
         # get the inputs
         inputs, labels = data
 
@@ -136,7 +112,7 @@ print('Finished Training')
 #
 # Okay, first step. Let us display an image from the test set to get familiar.
 
-dataiter = iter(testloader)
+dataiter = iter(test_loader)
 images, labels = dataiter.next()
 
 # print images
@@ -166,7 +142,7 @@ print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
 correct = 0
 total = 0
 with torch.no_grad():
-    for data in testloader:
+    for data in test_loader:
         images, labels = data
         outputs = net(images)
         _, predicted = torch.max(outputs.data, 1)
@@ -187,7 +163,7 @@ print('Accuracy of the network on the 10000 test images: %d %%' % (
 class_correct = list(0. for i in range(10))
 class_total = list(0. for i in range(10))
 with torch.no_grad():
-    for data in testloader:
+    for data in test_loader:
         images, labels = data
         outputs = net(images)
         _, predicted = torch.max(outputs, 1)
