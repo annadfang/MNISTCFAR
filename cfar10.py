ec3a7b25701b9fafa845131d80e7a89c0aaea7b9
Anna Michelle Fang <amf269@en-cs-nikola-compute06.coecis.cornell.edu>
2018-05-01 12:09:00 -0400

    finished training

--- cfar10.py
+++ cfar10.py
@@ -1,63 +1,61 @@
+
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
 import torch
 import torchvision
 import torchvision.transforms as transforms
+import torch.optim as optim
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 trainloader = torch.utils.data.DataLoader(
-    torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        transform=transform), 
-    batch_size=4, shuffle=True, num_workers=2)
+    torchvision.datasets.CIFAR10(root='./data', train=True, download = True, transform=transform), 
+    batch_size=64, shuffle=True, num_workers=1)
 
 testloader = torch.utils.data.DataLoader(
-    torchvision.datasets.CIFAR10(root='./data', train=False,
-                                       transform=transform), 
-    batch_size=4, shuffle=False, num_workers=2)
+    torchvision.datasets.CIFAR10(root='./data', train=False, download = True, transform=transform), 
+    batch_size=64, shuffle=True, num_workers=1)
 
-classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+classes = ('airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-from torch.autograd import Variable
-import torch.nn as nn
-import torch.nn.functional as F
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.dropout = nn.Dropout2d()
+        self.fc1 = nn.Linear(400, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.dropout(self.conv2(x)), 2))
+        x = x.view(-1, 400)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
-        return x
-
+        return F.log_softmax(x, dim = 1)
 
 model = Net()
-
-import torch.optim as optim
-
 optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
 def train(epoch):
     model.train()
     for batch_idx, (data, target) in enumerate (trainloader):
         data, target = Variable(data), Variable(target)
-        optimizer.zero_grad()
+        optimizer.zero_grad() #clear gradients
+        # each time variable is back propagated, the gradient will accumulate instead of replaced
         output = model(data)
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
+        if (batch_idx % 10 == 0):
+            print('Training Epoch: ', epoch, '  Loss: ', loss.data[0])
 def test():
     model.eval()
     test_loss = 0
@@ -75,6 +73,6 @@ def test():
         100. * correct / len(testloader.dataset)))
 
 
-for epoch in range(1, 10):
+for epoch in range(10):
     train(epoch)
     test()
