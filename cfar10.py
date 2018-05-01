05164531905b16fbf839f04a934dafbfdab21e40
Anna Fang <amf269@cornell.edu>
2018-04-30 21:45:46 -0400

    removed a layer

--- cfar10.py
+++ cfar10.py
@@ -36,15 +36,13 @@ class Net(nn.Module):
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
         self.fc1 = nn.Linear(16*5*5,120)
         self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84,10)
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
         x = x.view(-1, 320)
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.fc2(x)
         return x
 
 
