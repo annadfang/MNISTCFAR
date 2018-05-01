d60607fc63a70d8802d1cfc21465d8b21e7cbce1
Anna Fang <amf269@cornell.edu>
2018-04-30 21:49:56 -0400

    d

--- cfar10.py
+++ cfar10.py
@@ -8,12 +8,12 @@ transform = transforms.Compose(
 
 trainloader = torch.utils.data.DataLoader(
     torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=True, transform=transform), 
+                                        transform=transform), 
     batch_size=4, shuffle=True, num_workers=2)
 
 testloader = torch.utils.data.DataLoader(
     torchvision.datasets.CIFAR10(root='./data', train=False,
-                                       download=True, transform=transform), 
+                                       transform=transform), 
     batch_size=4, shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat',
